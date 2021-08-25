4d37a2198b5009ecd2b237316ee7a511c21abbc6
cyril <aguvasucyril@gmail.com>
2021-08-25 16:16:14 +0300

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'dotunj/randomable',
+  ),
+  'versions' => 
+  array (
+    'dotunj/randomable' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'fzaninotto/faker' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.9.x-dev',
+      ),
+      'reference' => '5ffe7db6c80f441f150fc88008d64e64af66634b',
+    ),
+  ),
+);
